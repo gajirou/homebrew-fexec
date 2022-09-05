c8d7a86644388a179b7ed872e1b8cb93c89af253
gaji <110142614+gajirou@users.noreply.github.com>
2022-09-05 12:05:21 +0900

    ハッシュ値修正
    ハッシュ値修正

--- fexec.rb
+++ fexec.rb
@@ -6,7 +6,7 @@ class Fexec < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gajirou/fexec/releases/download/v0.1.0/fexec_darwin_arm64.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "98fbf63d6eee32f1c17df0a376c714dc91d45e01a81fb37a4390e7a42f193c9c"
 
       def install
         bin.install "fexec"
@@ -14,7 +14,7 @@ class Fexec < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/gajirou/fexec/releases/download/v0.1.0/fexec_darwin_amd64.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "a1408506c224df3cda471ba2d9142349873f59e471486287a8986582ea95185a"
 
       def install
         bin.install "fexec"
